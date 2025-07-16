8eae9508ac47f379e0c82ced55324ee15f06d225
Joseph White <jpwhite4@buffalo.edu>
2025-07-16 14:34:37 -0400

    Syntax

--- tests/unit/test_resources.py
+++ tests/unit/test_resources.py
@@ -5,16 +5,19 @@ from xdmod_data.warehouse import DataWarehouse
 
 VALID_XDMOD_HOST = os.environ['XDMOD_HOST']
 
+
 class MockResponse:
+
     def __init__(self, status_code):
         self.status_code = status_code
         self.text = '{ "results": "resourcelist" }'
+
 
 @patch.object(Session, 'get', return_value=MockResponse(200))
 def test_fetch(self):
     with DataWarehouse(VALID_XDMOD_HOST) as dw:
         result = dw.get_resources()
-        assert "resourcelist" == result
+        assert 'resourcelist' == result
 
-        result = dw.get_resources("Provider")
-        assert "resourcelist" == result
+        result = dw.get_resources('Provider')
+        assert 'resourcelist' == result
