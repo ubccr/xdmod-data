e303208cff37696fa35a0d4bfdee33df55681792
Aaron Weeden <31246768+aaronweeden@users.noreply.github.com>
2025-07-17 15:03:21 -0400

    Add report module. (#85)
    Co-authored-by: Joseph White <jpwhite4@buffalo.edu>

---
+++ tests/unit/test_report_unit.py
@@ -0,0 +1,43 @@
+from IPython.display import Markdown, HTML
+import pytest
+from xdmod_data.report import set_styles, header, footer
+
+
+def test_set_styles():
+    style_html = set_styles()
+    assert isinstance(style_html, HTML)
+
+
+def test_header():
+    header_html = header()
+    assert isinstance(header_html, HTML)
+
+
+@pytest.mark.parametrize(
+    'docmeta',
+    [
+        None,
+        {
+            'title': 'ACCESS Utilization',
+            'version': 3,
+            'description': (
+                'This report describes utilization information for'
+                + ' ACCESS-allocated compute resources.'
+            ),
+            'history': [
+                ['1', '2024-10-23', 'Initial Version.'],
+                ['2', '2025-01-15', 'Updated time range for plots.'],
+                [
+                    '3',
+                    '2025-04-03',
+                    'Fix typographic errors, update plot times and'
+                    + ' resource specification for Stampede2.',
+                ],
+            ],
+        },
+    ],
+    ids=['without_docmeta', 'with_docmeta'],
+)
+def test_footer(docmeta):
+    footer_markdown = footer(docmeta)
+    assert isinstance(footer_markdown, Markdown)
